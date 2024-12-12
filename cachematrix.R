f83d93a15b5ec2f9ceb3a944125ec7d7074a56ce
tsuki03220 <1462694465@qq.com>
2024-12-12 18:52:56 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,7 +7,7 @@ makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
                 x <<- y
-                nv <<- NULL
+                inv <<- NULL
         }
         get <- function() x
         setInverse <- function(inverse) inv <<- inverse
